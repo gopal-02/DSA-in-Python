2f7195101ee0b64fc028f094f7850e14b8de73dd
gopal-02 <gopalkumar8220@gmail.com>
2026-02-09 22:27:37 +0530

    implement factorial using recursion

--- factorial.py
+++ factorial.py
@@ -1,15 +1,30 @@
-# Program to find the factorial of a number
-def factorial(n):
+# # Program to find the factorial of a number
+# def factorial(n):
+#     if n < 0:
+#         return "Factorial is not defined for negative numbers."
+#     elif n == 0 or n == 1:
+#         return 1
+#     else:
+#         result = 1
+#         for i in range(2, n + 1):
+#             result *= i
+#         return result
+# # Get user input
+# number = int(input("Enter a number to find its factorial: "))
+# # Calculate and print the factorial
+# print(f"The factorial of {number} is: {factorial(number)}")
+
+
+# factorial of a number using recursion
+def factorial_recursive(n):
     if n < 0:
         return "Factorial is not defined for negative numbers."
     elif n == 0 or n == 1:
         return 1
     else:
-        result = 1
-        for i in range(2, n + 1):
-            result *= i
-        return result
+        return n * factorial_recursive(n - 1)
+    
 # Get user input
-number = int(input("Enter a number to find its factorial: "))
+number = int(input("Enter a number to find its factorial using recursion: "))
 # Calculate and print the factorial
-print(f"The factorial of {number} is: {factorial(number)}")
+print(f"The factorial of {number} is: {factorial_recursive(number)}")
